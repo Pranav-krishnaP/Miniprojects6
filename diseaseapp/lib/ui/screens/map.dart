5d5d81f0e3e46fc823a8c453b6f77672b4c54554
Pranav <fifapranav3404@gmail.com>
2023-06-04 23:33:47 +0530

    Commit

--- diseaseapp/lib/ui/screens/map.dart
+++ diseaseapp/lib/ui/screens/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Map extends StatefulWidget {
   const Map({Key? key}) : super(key: key);
@@ -11,10 +13,53 @@ class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        title: const Text('New Post'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Redirect to Google Maps"),
+            ElevatedButton(
+              onPressed: () {
+                openGoogleMaps();
+              },
+              child: Text('Show Agriculture related shops near me'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+void openGoogleMaps() async {
+  // Check if the Google Maps app is installed
+  if (await canLaunchUrlString('comgooglemaps://')) {
+    // Construct the search query with the current location
+    final Uri googleMapsUri = Uri(
+      scheme: 'https',
+      host: 'www.google.com',
+      path: '/maps/search/',
+      queryParameters: {
+        'api': '1',
+        'query': 'Agriculture related shops near me',
+      },
+    );
+
+    // Open Google Maps with the search query
+    await launchUrlString(googleMapsUri.toString());
+  } else {
+    // Fallback to opening the Google Maps website
+    final Uri googleMapsUri = Uri(
+      scheme: 'https',
+      host: 'www.google.com',
+      path: '/maps/search/',
+      queryParameters: {
+        'api': '1',
+        'query': 'Agriculture shops near me',
+      },
+    );
+
+    // Open Google Maps website with the search query
+    await launchUrlString(googleMapsUri.toString());
   }
 }
