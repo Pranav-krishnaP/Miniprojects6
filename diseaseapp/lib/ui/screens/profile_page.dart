f383abee65e601fc83cd0573ff8807cf848416b1
Philipjohn100 <philipjohn9950@gmail.com>
2023-06-21 23:30:07 +0530

    new commit

--- diseaseapp/lib/ui/screens/profile_page.dart
+++ diseaseapp/lib/ui/screens/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:share_plus/share_plus.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({Key? key}) : super(key: key);
@@ -23,15 +24,25 @@ class ProfilePage extends StatelessWidget {
               SettingsTile(
                 title: Text('Support'),
                 leading: Icon(Icons.support_agent),
-                onPressed: (BuildContext context) {
-                  _navigateToNextScreen2(context);
+                onPressed: (BuildContext) async {
+                  String email = Uri.encodeComponent("agrovzn@gmail.com");
+                  String subject = Uri.encodeComponent("");
+                  String body = Uri.encodeComponent("");
+                  print(subject); //output: Hello%20Flutter
+                  Uri mail =
+                      Uri.parse("mailto:$email?subject=$subject&body=$body");
+                  if (await launchUrl(mail)) {
+                    //email app opened
+                  } else {
+                    //email app is not opened
+                  }
                 },
               ),
               SettingsTile(
                 title: Text('share'),
                 leading: Icon(Icons.share),
                 onPressed: (BuildContext context) {
-                  _navigateToNextScreen3(context);
+                  Share.share("com.example.AgroVision");
                 },
               ),
             ],
@@ -49,11 +60,6 @@ class ProfilePage extends StatelessWidget {
   void _navigateToNextScreen2(BuildContext context) {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => suppotrt()));
-  }
-
-  void _navigateToNextScreen3(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => share()));
   }
 }
 
@@ -77,21 +83,6 @@ class suppotrt extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Support')),
-      body: const Center(
-        child: Text(
-          'This is a new screen',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
-    );
-  }
-}
-
-class share extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Share')),
       body: const Center(
         child: Text(
           'This is a new screen',
--- diseaseapp/lib/ui/screens/widgets/weather.dart
+++ diseaseapp/lib/ui/screens/widgets/weather.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+// import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: FutureBuilder(
-        future: getWeatherbitWidget(),
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          if (snapshot.hasData) {
-            return HtmlWidget(snapshot.data.toString());
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    ),
-  ));
-}
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       body: FutureBuilder(
+//         future: getWeatherbitWidget(),
+//         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+//           if (snapshot.hasData) {
+//             return HtmlWidget(snapshot.data.toString());
+//           } else {
+//             return Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//     ),
+//   ));
+// }
 
-Future<String> getWeatherbitWidget() async {
-  String apiKey = 'c376723d8a9f4e45b8d187c5a116f88a';
-  String url =
-      'https://weatherbit.io/widgets/current/weather-embed?lang=en&location=auto-full&theme=dark&api_key=$apiKey';
-  http.Response response = await http.get(Uri.parse(url));
-  return response.body;
-}
+// Future<String> getWeatherbitWidget() async {
+//   String apiKey = 'c376723d8a9f4e45b8d187c5a116f88a';
+//   String url =
+//       'https://weatherbit.io/widgets/current/weather-embed?lang=en&location=auto-full&theme=dark&api_key=$apiKey';
+//   http.Response response = await http.get(Uri.parse(url));
+//   return response.body;
+// }
